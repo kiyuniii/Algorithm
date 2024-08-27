05c7e7f98db4c6e216665b30a2435365f52387cd
KIYUN <bkyun3012@sju.ac.kr>
2024-08-27 22:04:45 +0900

    file list edit

--- BASIC/Euclidean.c
+++ BASIC/Euclidean.c
@@ -27,6 +27,16 @@ int Euclidean1(int a, int b) {
     return a;
 }
 
+/* int gcd(int a, int b) {
+    while(b != 0) {
+        int r = a%b;
+        a = b;
+        b = r;
+    }
+    return a;
+}
+ */
+
 int main() {
     int a, b;
     scanf("%d%d", &a, &b);
